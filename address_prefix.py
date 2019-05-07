9575ebf8f47fffd79e192906223f3130a30ccb82
Seungjun <seungjun45@kaist.ac.kr>
2019-05-07 13:35:08 +0900

    address

--- address_prefix.py
+++ address_prefix.py
@@ -6,8 +6,8 @@ import argparse
 if(platform.system() == 'Linux'):
     COCO_IMG_PATH = "Data_Share/Datas/VQA_COCO"
     YOLO_LABEL_PATH = "Data_Share/Datas/VQA_COCO"
-    addr_prefix="../../"+COCO_IMG_PATH
-    addr_label_prefix="../../"+YOLO_LABEL_PATH
+    addr_prefix="../../../"+COCO_IMG_PATH
+    addr_label_prefix="../../../"+YOLO_LABEL_PATH
 elif (platform.system() == 'Windows'):
     COCO_IMG_PATH = "D:\\Data_Share\Datas\VQA_COCO"
     YOLO_LABEL_PATH = "D:\\Data_Share\Datas\VQA_COCO"
