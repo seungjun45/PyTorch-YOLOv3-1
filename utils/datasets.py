91b47d20b900a94e5caa8d8693ee7723c552c0c3
Seungjun <seungjun45@kaist.ac.kr>
2019-06-10 17:25:46 +0900

    GTA

--- utils/datasets.py
+++ utils/datasets.py
@@ -14,6 +14,7 @@ import torchvision.transforms as transforms
 import pdb
 from PIL import ImageFile
 ImageFile.LOAD_TRUNCATED_IMAGES = True
+import json
 
 
 def pad_to_square(img, pad_value):
@@ -68,6 +69,119 @@ class ListDataset(Dataset):
         with open(list_path, "r") as file:
             self.img_files = file.readlines()
 
+        self.label_files = [addr_label_prefix+path.replace("images", "labels").replace(".png", ".txt").replace(".jpg", ".txt")
+            for path in self.img_files]
+        self.img_files = [addr_prefix + path for path in self.img_files]
+        self.img_size = img_size
+        self.max_objects = 100
+        self.augment = augment
+        self.multiscale = multiscale
+        self.normalized_labels = normalized_labels
+        self.min_size = self.img_size - 3 * 32
+        self.max_size = self.img_size + 3 * 32
+        self.batch_count = 0
+
+    def __getitem__(self, index):
+
+        # ---------
+        #  Image
+        # ---------
+
+        img_path = self.img_files[index % len(self.img_files)].rstrip()
+
+        # Extract image as PyTorch tensor
+        img = transforms.ToTensor()(Image.open(img_path).convert('RGB'))
+
+        # Handle images with less than three channels
+        if len(img.shape) != 3:
+            img = img.unsqueeze(0)
+            img = img.expand((3, img.shape[1:]))
+
+        _, h, w = img.shape
+        h_factor, w_factor = (h, w) if self.normalized_labels else (1, 1)
+        # Pad to square resolution
+        img, pad = pad_to_square(img, 0)
+        _, padded_h, padded_w = img.shape
+
+        # ---------
+        #  Label
+        # ---------
+
+        label_path = self.label_files[index % len(self.img_files)].rstrip()
+
+        targets = None
+        if os.path.exists(label_path):
+            boxes = torch.from_numpy(np.loadtxt(label_path).reshape(-1, 5))
+            # Extract coordinates for unpadded + unscaled image
+            x1 = w_factor * (boxes[:, 1] - boxes[:, 3] / 2)
+            y1 = h_factor * (boxes[:, 2] - boxes[:, 4] / 2)
+            x2 = w_factor * (boxes[:, 1] + boxes[:, 3] / 2)
+            y2 = h_factor * (boxes[:, 2] + boxes[:, 4] / 2)
+            # Adjust for added padding
+            x1 += pad[0]
+            y1 += pad[2]
+            x2 += pad[1]
+            y2 += pad[3]
+            # Returns (x, y, w, h)
+            boxes[:, 1] = ((x1 + x2) / 2) / padded_w
+            boxes[:, 2] = ((y1 + y2) / 2) / padded_h
+            boxes[:, 3] *= w_factor / padded_w
+            boxes[:, 4] *= h_factor / padded_h
+
+            targets = torch.zeros((len(boxes), 6))
+            targets[:, 1:] = boxes
+
+            # Apply augmentations
+            if self.augment:
+                if np.random.random() < 0.5:
+                    # try:
+                    img, targets = horisontal_flip(img, targets)
+                    # except:
+                    #     print('==============')
+                    #     print(label_path)
+                    #     print('==============')
+                    #     pdb.set_trace()
+        return img_path, img, targets
+
+    def collate_fn(self, batch):
+        paths, imgs, targets = list(zip(*batch))
+        # Remove empty placeholder targets
+        new_targets=[]
+        new_imgs=[]
+        new_paths=[]
+        for (path,img,boxes) in zip(paths,imgs,targets):
+            if boxes is not None:
+                new_targets.append(boxes)
+                new_imgs.append(img)
+                new_paths.append(path)
+        imgs=new_imgs
+        paths=new_paths
+        targets=new_targets
+        # Add sample index to targets
+        for i, boxes in enumerate(targets):
+            boxes[:, 0] = i
+        targets = torch.cat(targets, 0)
+        # Selects new image size every tenth batch
+        if self.multiscale and self.batch_count % 10 == 0:
+            self.img_size = random.choice(range(self.min_size, self.max_size + 1, 32))
+        # Resize images to input shape
+
+        # Considering Gray-Scale Images... I changed codes a little bit
+        tmp_list=[]
+        for img in imgs:
+            img=resize(img, self.img_size)
+            tmp_list.append(img)
+        imgs=torch.stack(tmp_list)
+        self.batch_count += 1
+        return paths, imgs, targets
+
+    def __len__(self):
+        return len(self.img_files)
+
+class GTADataSet(Dataset):
+    def __init__(self, json_path, img_size=416, augment=True, multiscale=True, normalized_labels=True):
+        json_=json.load(open(json_path))
+        pdb.set_trace()
         self.label_files = [addr_label_prefix+path.replace("images", "labels").replace(".png", ".txt").replace(".jpg", ".txt")
             for path in self.img_files]
         self.img_files = [addr_prefix + path for path in self.img_files]
